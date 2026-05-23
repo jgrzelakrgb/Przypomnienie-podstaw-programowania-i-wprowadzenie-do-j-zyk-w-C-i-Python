a3653be4322a09b68660c3bc543bcf073a0294a2
kuban <kuban@ASUSJAKUB>
2026-05-23 17:56:11 +0200

    Dodaj pliki projektów.

---
+++ zad 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+static void Main()
+{
+Console.Write("Podaj pierwszą liczbę: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Podaj drugą liczbę: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Podaj działanie (+, -, *, /): ");
+string znak = Console.ReadLine();
+
+if (znak == "+")
+{
+Console.WriteLine("Wynik: " + (a + b));
+}
+else if (znak == "-")
+{
+ Console.WriteLine("Wynik: " + (a - b));
+}
+else if (znak == "*")
+{
+Console.WriteLine("Wynik: " + (a * b));
+}
+else if (znak == "/")
+{
+Console.WriteLine("Wynik: " + (a / b));
+}
+else
+{
+Console.WriteLine("Błędne działanie");
+}
+}
+}
