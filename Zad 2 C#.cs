1c76592d1aa476230360f236d01575c6c5128c4a
jgrzelakrgb <j.grzelak2002@gmail.com>
2026-05-23 18:31:08 +0200

    Add files via upload

---
+++ Zad 2 C#.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Konwerter temperatur");
+
+        Console.Write("Wybierz C lub F: ");
+        string wybor = Console.ReadLine();
+
+        if (wybor == "C")
+        {
+            Console.Write("Podaj temperature w Celsjuszach: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double f = c * 1.8 + 32;
+
+            Console.WriteLine("Temperatura w Fahrenheitach = " + f);
+        }
+        else if (wybor == "F")
+        {
+            Console.Write("Podaj temperature w Fahrenheitach: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            double c = (f - 32) / 1.8;
+
+            Console.WriteLine("Temperatura w Celsjuszach = " + c);
+        }
+        else
+        {
+            Console.WriteLine("Bledny wybor");
+        }
+    }
+}
